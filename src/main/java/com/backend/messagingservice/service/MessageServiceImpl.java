646f149831003edb6afed00e0fd4939bbe0d3ca8
taylorhoyt <31395207+taylorhoyt@users.noreply.github.com>
2023-08-24 19:59:01 -0500

    Added properties for to/from email addresses

--- src/main/java/com/backend/messagingservice/service/MessageServiceImpl.java
+++ src/main/java/com/backend/messagingservice/service/MessageServiceImpl.java
@@ -2,6 +2,7 @@ package com.backend.messagingservice.service;
 
 import com.backend.messagingservice.object.UserMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,14 @@ public class MessageServiceImpl implements MessageService{
     @Autowired
     private JavaMailSender messageSender;
 
+    @Autowired
+    private Environment environment;
+
     private SimpleMailMessage buildMessage(UserMessage rawMessage){
         SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom("{user email}");
-        message.setTo("{email to send to}");
+        message.setFrom(environment.getProperty("backend.messagingservice.sendFrom"));
+        message.setTo(environment.getProperty("backend.messagingservice.sendTo"));
         message.setSubject("Message from " + rawMessage.getName());
         message.setText(rawMessage.getMessage() + "\n Sent on " + rawMessage.getDateAndTime());
 
